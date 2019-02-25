afb7e3c8b942383f76e23ee78b4b4a0bbd82d510
mariah <mariah@gardziola.com>
2019-02-25 16:14:23 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,10 +12,10 @@ end
 
 def valid_move?(board,index)
   if index.between?(0,8) && !position_taken?(board,index)
-return true
-else
-return false
-end
+    return true
+  else
+    return false
+  end
 end
 
 def position_taken?(board, index)
