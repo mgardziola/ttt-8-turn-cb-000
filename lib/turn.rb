51e8657a2456332a16951578a8cbeaa275577fd0
mariah <mariah@gardziola.com>
2019-02-25 16:07:29 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -20,7 +20,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if position_taken?(board, index) == false && index <= 8
+  if position_taken?[board, index] == false && index <= 8
     return true
   end
 end
